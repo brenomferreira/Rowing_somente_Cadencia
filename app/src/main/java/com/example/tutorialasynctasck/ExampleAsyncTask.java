f93c5fe4e79c1ad561ed71e6fc1be97bc954c2cb
brenomferreira <brenomferreira@hotmail.com>
2019-05-22 10:12:40 -0300

    anotaçoes na classe

--- app/src/main/java/com/example/tutorialasynctasck/ExampleAsyncTask.java
+++ app/src/main/java/com/example/tutorialasynctasck/ExampleAsyncTask.java
@@ -11,62 +11,60 @@ public class ExampleAsyncTask extends AsyncTask<Integer, Integer, String> {
     private WeakReference<MainActivity> activityWeakReference;
     ProgressBar progressBar;
 
-
+    // CONSTRUTOR
     ExampleAsyncTask(MainActivity activity, ProgressBar progressBar) {
         activityWeakReference = new WeakReference<MainActivity>(activity);
         this.progressBar = progressBar;
-    }
+    }// Fim do CONSTRUTOR
 
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
-
         MainActivity activity = activityWeakReference.get();
+
         if (activity == null || activity.isFinishing()) {
             return;
         }
-
         progressBar.setVisibility(View.VISIBLE);
-    }
+    }// Fim onPreExecute
 
     @Override
     protected String doInBackground(Integer... integers) {
         for (int i = 0; i < integers[0]; i++) {
             publishProgress((i * 100) / integers[0]);
+
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-
         return "Finished!";
-    }
+    }// Fim doInBackground
 
     @Override
     protected void onProgressUpdate(Integer... values) {
         super.onProgressUpdate(values);
-
         MainActivity activity = activityWeakReference.get();
+
         if (activity == null || activity.isFinishing()) {
             return;
         }
-
         progressBar.setProgress(values[0]);
-    }
+    }// Fim onProgressUpdate
 
     @Override
     protected void onPostExecute(String s) {
         super.onPostExecute(s);
-
         MainActivity activity = activityWeakReference.get();
+
         if (activity == null || activity.isFinishing()) {
             return;
         }
-
         Toast.makeText(activity, s, Toast.LENGTH_SHORT).show();
         progressBar.setProgress(0);
         progressBar.setVisibility(View.INVISIBLE);
-    }
-}
+    }// Fim onPostExecute
+
+}// Fim da Classe ExampleAsyncTask
 
